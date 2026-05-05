be75d404ab60e71c36eaef905e1688d70959232f
AxelCastroo <acastrobenza@itba.edu.ar>
2026-05-04 23:49:44 -0300

    feat: add "back to menu" button on game over screen

--- Assets/UI/UI_Gameover.cs
+++ Assets/UI/UI_Gameover.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UI_Gameover : MonoBehaviour
 {
+    private const string MENU_SCENE_NAME = "Menu";
+
     [SerializeField] private Sprite _victory;
     [SerializeField] private Sprite _defeat;
     [SerializeField] private Image _gameoverImage;
+
+    [Header("Back to menu")]
+    [SerializeField] private Button _backToMenuButton;
 
     #region UNITY_EVENTS
     private void Start()
     {
         _gameoverImage.enabled = false;
+
+        if (_backToMenuButton != null)
+        {
+            _backToMenuButton.gameObject.SetActive(false);
+            _backToMenuButton.onClick.AddListener(LoadMenu);
+        }
 
         GameoverSuscribe();
     }
@@ -20,6 +30,9 @@ public class UI_Gameover : MonoBehaviour
     private void OnDestroy()
     {
         GameoverUnsuscribe();
+
+        if (_backToMenuButton != null)
+            _backToMenuButton.onClick.RemoveListener(LoadMenu);
     }
     #endregion
 
@@ -31,6 +44,19 @@ public class UI_Gameover : MonoBehaviour
     {
         _gameoverImage.enabled = true;
         _gameoverImage.sprite = isVictory ? _victory : _defeat;
+
+        if (_backToMenuButton != null)
+            _backToMenuButton.gameObject.SetActive(true);
+    }
+    #endregion
+
+    #region BACK_TO_MENU
+    private void LoadMenu()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        SceneManager.LoadScene(MENU_SCENE_NAME);
     }
     #endregion
 }
